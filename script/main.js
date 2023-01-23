7bb904e45055d8e992198b4aeb73f74604a1b967
HedaKS <118710428+HedaKS@users.noreply.github.com>
2023-01-23 14:14:13 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,35 +1,6 @@
 
-
-
-
-// // Obtenir une référence au bouton et au conteneur de citation
-// const button = document.querySelector("#button");
-// const divcitation = document.querySelector("#citation");
-
-// // Obtenir une citation lorsque le bouton est cliqué
-// button.addEventListener("click", newcitation);
-
-// // Obtenir une citation toutes les 5 secondes
-// setInterval(newcitation, 5000);
-
-// // Mes citation
-// let citations = [
-//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget rutrum quam, non tincidunt augue. Proin hendrerit, ligula eget auctor bibendum, neque neque ornare mi, eu luctus nibh felis eu neque. Nam id orci eu magna tristique imperdiet sit amet nec purus. Maecenas pharetra faucibus sollicitudin. Vestibulum commodo nisi vitae augue mollis viverra.1", 
-// "Ut vehicula sit amet leo vitae rutrum. Fusce tincidunt nulla erat, vitae tincidunt enim pretium vitae. Fusce non nulla a purus mattis ullamcorper.", 
-// "Donec congue nunc turpis, non tempor metus tempus sit amet. Morbi ullamcorper interdum elit, in elementum nisl cursus ut. Vivamus convallis non nunc id hendrerit."
-// ];
-
-// function newcitation() {
-//   // Sélectionner une citation aléatoire à partir des données
-//   const random = Math.floor(Math.random() * citations.length);
-//   const citation = citations[random];
-
-//   // Afficher la citation dans le conteneur de citation
-//   divcitation.innerHTML = citation;
-// }
-
-
 const button = document.getElementById('button');
+const stopbutton = document.getElementById('stopButton');
 const citation = document.getElementById('citation');
 
 // Fonction pour afficher une citation aléatoire
@@ -37,19 +8,37 @@ function randomcitation() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
         .then(response => response.json())
         .then(data => {
-            // Sélectionne une citation aléatoire
+            // Sélectionne un pokemon aléatoire
             const randomIndex = Math.floor(Math.random() * data.results.length);
-            const randomQuote = data.results[randomIndex].name;
-
-            // Affiche la citation dans le conteneur
-            citation.innerText = randomQuote;
+            const randomPokemon = data.results[randomIndex];
+            const pokemonId = randomPokemon.url.split('/')[randomPokemon.url.split('/').length - 2];
+        
+            // Affiche le nom du pokemon dans le conteneur
+            citation.innerText = randomPokemon.name;
+        
+            //Ajout l'image de pokemon dans l'élément img
+            const pokemonImg = document.querySelector("#pokemonImg");
+            pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+            pokemonImg.alt = randomPokemon.name;
         });
 }
 
+function showbloc(){
+    citation.style.display = "flex";
+}
+
 // Affiche une citation au clic sur le bouton
-button.addEventListener('click', randomcitation);
+button.addEventListener('click', function(){ 
+    randomcitation();
+    showbloc();
+    clearInterval(intervalId);
+    intervalId = setInterval(randomcitation, 100);
+ });
 
 // Affiche une citation toutes les 5 secondes
-setInterval(randomcitation, 5000);
+let intervalId = setInterval(randomcitation, 100);
 
 
+stopbutton.addEventListener("click", function(){
+    clearInterval(intervalId);
+});
